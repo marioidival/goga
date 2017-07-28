2076e77dd35bd3e63c1b989e084c358c50588cef
Mario Idival <marioidival@gmail.com>
2017-07-28 10:17:26 -0300

    Add tests to ext_where

--- src/postgres/commands.rs
+++ src/postgres/commands.rs
@@ -1,4 +1,43 @@
-pub fn query_operator(qop: &String) -> Result<String, String> {
+extern crate rocket;
+
+use rocket::{Request, Data, Outcome};
+use rocket::data::{self, FromData};
+use rocket::Outcome::*;
+use std::collections::HashMap;
+
+
+pub struct WhereStructResult {
+    sql: String, values: Vec<String>,
+}
+
+// My Results
+type QueryOpResult = Result<String, String>;
+type WhereResult = Result<WhereStructResult, String>;
+
+// implement QueryString
+pub struct QueryString {
+    query: HashMap<String, String>
+}
+
+impl FromData for QueryString {
+    type Error = String;
+
+    fn from_data(req: &Request, _: Data) -> data::Outcome<Self, String> {
+        let querystring: Vec<&str> = req.uri().query()
+            .unwrap().split("&").collect();
+        let mut result: QueryString = QueryString{query: HashMap::new()};
+
+        for qs in querystring {
+            let kv: Vec<&str> = qs.split("=").collect();
+            // [0] is key, [1] is value
+            result.query.insert(kv[0].to_string(), kv[1].to_string());
+        }
+        Success(result)
+    }
+}
+
+
+pub fn query_operator(qop: &String) -> QueryOpResult {
     match &qop[..] {
         "$eq" => Ok("=".to_string()),
         "$ne" => Ok("!=".to_string()),
@@ -14,12 +53,24 @@ pub fn query_operator(qop: &String) -> Result<String, String> {
     }
 }
 
+// Get request and return chuck WHERE statement with values
+pub fn ext_where(_: &Request) -> WhereResult {
+    Ok(WhereStructResult {
+        sql: "WHERE user_id=$1".to_string(),
+        values: vec!["5".to_string()],
+    })
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::{rocket};
+    use rocket::local::Client;
+    use rocket::http::ContentType;
     use postgres::commands::*;
 
     #[test]
-    fn it_get_opetators() {
+    fn get_opetators() {
         use std::collections::HashMap;
 
         let mut test_cases = HashMap::new();
@@ -43,12 +94,24 @@ mod tests {
     }
 
     #[test]
-    fn it_get_opetators_error() {
+    fn get_opetators_error() {
         let invalid = "!tn".to_string();
         match query_operator(&invalid) {
             Ok(_) => println!("no result"),
             Err(e) => assert_eq!(format!("operator {} not found", invalid), e)
         }
-        // assert_eq!(expected, query_operatorv2(&invalid));
+    }
+
+    #[test]
+    fn where_by_request() {
+        let client = Client::new(rocket::ignite())
+            .expect("valid rocket");
+        let req = client.get("/db/sch/tbl?user_id=5&name=cool")
+            .header(ContentType::JSON);
+        let result = ext_where(req.inner())
+            .unwrap();
+
+        assert_eq!(result.sql, "WHERE user_id=$1".to_string());
+        assert_eq!(result.values, vec!["5".to_string()])
     }
 }
